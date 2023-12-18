89876f6147c54f1a28e13d84239a57561004ad70
gh0sty02 <pranayyadav08@gmail.com>
2023-12-18 16:14:33 +0530

    added org list sidebar

---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { Accordion } from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { useMemo } from "react";
+import { useLocalStorage } from "usehooks-ts";
+
+interface sideBarProps {
+  storageKey?: string;
+}
+
+const Sidebar = ({ storageKey = "flowify-sidebar-state" }: sideBarProps) => {
+  const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
+    storageKey,
+    {}
+  );
+
+  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
+    useOrganization();
+
+  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+    userMemberships: {
+      infinite: true,
+    },
+  });
+
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
+
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({ ...curr, [id]: !expanded[id] }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">Workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+          <p key={organization.id}>{organization.id}</p>
+        ))}
+      </Accordion>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { OrgControl } from "./_components/org-control";
+
+export default function OrganizationIdLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+}
